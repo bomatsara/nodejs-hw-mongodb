6101fb4e08d1b9eb9e1dad0bf24a6a9e7f202680
Andrii Bomatsara <andrii.bomatsara@vista.com>
2024-10-13 15:48:57 +0300

    Do hometask

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import router from './routers/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
+import swaggerDocs from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -34,6 +35,7 @@ export const setupServer = () => {
   });
 
   app.use(router);
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFoundHandler);
   app.use(errorHandler);
